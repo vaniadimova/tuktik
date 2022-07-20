85882153839a73f7f55da32e394d07a9a2369d17
Vania Dimova <ivankadimov8@gmail.com>
2022-07-20 15:28:48 -0400

    auth update

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,14 +1,12 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { allUsersQuery } from './../../utils/queries';
 import { client } from '../../utils/client';
 
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const doc = req.body;
 
-export default async function handler(req: NextApiRequest,
-     res: NextApiResponse) {
-     if (req.method === 'POST') {
-    const user = req.body;
-
-    client.createIfNotExists(user)
-    .then(() => res.status(200).json( 'Login Success!' ))
-
-}
+  client.createIfNotExists(doc).then(() => {
+    res.status(200).json('Login successful');
+  });
 }
